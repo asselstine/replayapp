276acda342825b2225dc58a4bec66ae0631f9752
Brendan Asselstine <mail.asselstine@gmail.com>
2017-10-12 13:33:11 -0700

    When videos are removed externally they are removed within the app
    [see #151927692]

--- shared/components/linked-videos-screen/linked-videos-container/index.js
+++ shared/components/linked-videos-screen/linked-videos-container/index.js
@@ -1,11 +1,14 @@
 import { connect } from 'react-redux'
 import { LinkedVideos } from './linked-videos'
+import { PhotosFramework } from '../../../services/photos-framework'
 import _ from 'lodash'
 
 export const LinkedVideosContainer = connect(
   (state, ownProps) => {
+    var videos = _.values(state.videos)
+    PhotosFramework.checkVideos(videos)
     return {
-      videos: _.sortBy(_.values(state.videos), ['creationDateUTCSeconds'])
+      videos: _.sortBy(videos, ['creationDateUTCSeconds'])
     }
   }
 )(LinkedVideos)
